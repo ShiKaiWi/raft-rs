97ffa1e511df0a13ca3103b86ac78a8ae980ca3b
Hoverbear <operator@hoverbear.org>
2018-09-12 08:00:42 -0700

    Introduce ConfigurationState.
    The ConfigurationState structure wraps around the Configuration and
    holds the optional next Configuration.

--- src/progress.rs
+++ src/progress.rs
@@ -48,9 +48,28 @@ impl Default for ProgressState {
 }
 
 #[derive(Clone, Debug, Default)]
-struct Configuration {
+pub struct Configuration {
     voters: FxHashSet<u64>,
     learners: FxHashSet<u64>,
+}
+
+impl<V, L> From<(V, L)> for Configuration
+where V: IntoIterator<Item=u64>, L: IntoIterator<Item=u64> {
+    fn from((voters, learners): (V, L)) -> Self {
+        Self {
+            voters: voters.into_iter().collect(),
+            learners: learners.into_iter().collect(),
+        }
+    }
+}
+
+impl Configuration {
+    fn with_capacity(voters: usize, learners: usize) -> Self {
+        Self {
+            voters: HashSet::with_capacity_and_hasher(voters, FxBuildHasher::default()),
+            learners: HashSet::with_capacity_and_hasher(learners, FxBuildHasher::default()),
+        }
+    }
 }
 
 /// The status of an election according to a Candidate node.
@@ -66,12 +85,103 @@ pub enum CandidacyStatus {
     Ineligible,
 }
 
+/// The configuration state of the `ProgressSet`. It is used to determine how quorums are accounted for.
+#[derive(Clone, Debug, Default)]
+pub struct ConfigurationState {
+    /// The cluster is not in a changing configuration. It has only one quorum. It is in a steady state.
+    current: Configuration,
+    /// The cluster is currently in a changing configuration. It has two seperate quorums which must agree before progressing.
+    next: Option<Configuration>,
+}
+
+impl ConfigurationState {
+    /// Create a new configuration state with the given sizes.
+    pub fn with_capacity(voters: usize, learners: usize) -> Self {
+        Self {
+            current: Configuration::with_capacity(voters, learners),
+            next: None,
+        }
+    }
+
+    /// Adds a voter node.
+    pub fn insert_voter(&mut self, id: u64) -> Result<(), Error> {
+        if self.learners().contains(&id) {
+            Err(Error::Exists(id, "learners"))?;
+        } else if self.voters().contains(&id) {
+            Err(Error::Exists(id, "voters"))?;
+        }
+        self.current.voters.insert(id);
+        self.next.as_mut().map(|config| config.voters.insert(id));
+        Ok(())
+    }
+
+    /// Returns the ids of all known voters.
+    #[inline]
+    pub fn voters(&self) -> FxHashSet<u64> {
+        match self.next {
+            Some(ref next) => self.current.voters.union(&next.voters).cloned().collect::<FxHashSet<u64>>(),
+            None => self.current.voters.clone(),
+        }
+    }
+
+    /// Returns the ids of all known learners.
+    #[inline]
+    pub fn learners(&self) -> FxHashSet<u64> {
+        match self.next {
+            Some(ref next) => self.current.learners.union(&next.learners).cloned().collect::<FxHashSet<u64>>(),
+            None => self.current.learners.clone(),
+        }
+    }
+
+    /// Promote a learner to a peer.
+    pub fn promote_learner(&mut self, id: u64) -> Result<(), Error> {
+        if self.voters().contains(&id) {
+            Err(Error::Exists(id, "voters"))?;
+        } else if !self.learners().contains(&id) {
+            Err(Error::NotExists(id, "learners"))?;
+        }
+
+        self.current.voters.insert(id);
+        self.current.learners.remove(&id);
+        if let Some(ref mut next) = self.next {
+            next.voters.insert(id);
+            next.learners.remove(&id);
+        }
+        Ok(())
+    }
+
+    /// Adds a learner to the cluster
+    pub fn insert_learner(&mut self, id: u64) -> Result<(), Error> {
+        if self.learners().contains(&id) {
+            Err(Error::Exists(id, "learners"))?;
+        } else if self.voters().contains(&id) {
+            Err(Error::Exists(id, "voters"))?;
+        }
+
+        self.current.learners.insert(id);
+        if let Some(ref mut next) = self.next {
+            next.learners.insert(id);
+        }
+        Ok(())
+    }
+
+    /// Removes the peer from the set of voters or learners.
+    pub fn remove(&mut self, id: u64) {
+        self.current.learners.remove(&id);
+        self.current.voters.remove(&id);
+        if let Some(ref mut next) = self.next {
+            next.learners.remove(&id);
+            next.voters.remove(&id);
+        };
+    }
+}
+
 /// `ProgressSet` contains several `Progress`es,
 /// which could be `Leader`, `Follower` and `Learner`.
 #[derive(Default, Clone)]
 pub struct ProgressSet {
     progress: FxHashMap<u64, Progress>,
-    configuration: Configuration,
+    configuration: ConfigurationState,
 }
 
 impl ProgressSet {
@@ -90,31 +200,28 @@ impl ProgressSet {
                 voters + learners,
                 FxBuildHasher::default(),
             ),
-            configuration: Configuration {
-                voters: HashSet::with_capacity_and_hasher(voters, FxBuildHasher::default()),
-                learners: HashSet::with_capacity_and_hasher(learners, FxBuildHasher::default()),
-            },
+            configuration: ConfigurationState::with_capacity(voters, learners),
         }
     }
 
     /// Returns the status of voters.
     #[inline]
     pub fn voters(&self) -> impl Iterator<Item = (&u64, &Progress)> {
-        let set = self.voter_ids().clone();
+        let set = self.configuration.voters();
         self.progress.iter().filter(move |(&k, _)| set.contains(&k))
     }
 
     /// Returns the status of learners.
     #[inline]
     pub fn learners(&self) -> impl Iterator<Item = (&u64, &Progress)> {
-        let set = self.learner_ids().clone();
+        let set = self.configuration.learners();
         self.progress.iter().filter(move |(&k, _)| set.contains(&k))
     }
 
     /// Returns the mutable status of voters.
     #[inline]
     pub fn voters_mut(&mut self) -> impl Iterator<Item = (&u64, &mut Progress)> {
-        let set = self.voter_ids().clone();
+        let set = self.configuration.voters();
         self.progress
             .iter_mut()
             .filter(move |(&k, _)| set.contains(&k))
@@ -123,7 +230,7 @@ impl ProgressSet {
     /// Returns the mutable status of learners.
     #[inline]
     pub fn learners_mut(&mut self) -> impl Iterator<Item = (&u64, &mut Progress)> {
-        let set = self.learner_ids().clone();
+        let set = self.configuration.learners();
         self.progress
             .iter_mut()
             .filter(move |(&k, _)| set.contains(&k))
@@ -131,14 +238,14 @@ impl ProgressSet {
 
     /// Returns the ids of all known voters.
     #[inline]
-    pub fn voter_ids(&self) -> &FxHashSet<u64> {
-        &self.configuration.voters
+    pub fn voter_ids(&self) -> FxHashSet<u64> {
+        self.configuration.voters()
     }
 
     /// Returns the ids of all known learners.
     #[inline]
-    pub fn learner_ids(&self) -> &FxHashSet<u64> {
-        &self.configuration.learners
+    pub fn learner_ids(&self) -> FxHashSet<u64> {
+        self.configuration.learners()
     }
 
     /// Grabs a reference to the progress of a node.
@@ -167,49 +274,27 @@ impl ProgressSet {
 
     /// Adds a voter node
     pub fn insert_voter(&mut self, id: u64, pr: Progress) -> Result<(), Error> {
-        if self.learner_ids().contains(&id) {
-            Err(Error::Exists(id, "learners"))?;
-        } else if self.voter_ids().contains(&id) {
-            Err(Error::Exists(id, "voters"))?;
-        }
-        self.configuration.voters.insert(id);
+        self.configuration.insert_voter(id)?;
         self.progress.insert(id, pr);
         Ok(())
     }
 
     /// Adds a learner to the cluster
     pub fn insert_learner(&mut self, id: u64, pr: Progress) -> Result<(), Error> {
-        if self.learner_ids().contains(&id) {
-            Err(Error::Exists(id, "learners"))?;
-        } else if self.voter_ids().contains(&id) {
-            Err(Error::Exists(id, "voters"))?;
-        }
-        self.configuration.learners.insert(id);
+        self.configuration.insert_learner(id)?;
         self.progress.insert(id, pr);
         Ok(())
     }
 
     /// Removes the peer from the set of voters or learners.
     pub fn remove(&mut self, id: u64) -> Option<Progress> {
-        self.configuration.voters.remove(&id);
-        self.configuration.learners.remove(&id);
+        self.configuration.remove(id);
         self.progress.remove(&id)
     }
 
     /// Promote a learner to a peer.
     pub fn promote_learner(&mut self, id: u64) -> Result<(), Error> {
-        let is_learner = self.learner_ids().contains(&id);
-        let is_voter = self.voter_ids().contains(&id);
-
-        if is_voter {
-            Err(Error::Exists(id, "voters"))
-        } else if is_learner {
-            self.configuration.voters.insert(id);
-            self.configuration.learners.remove(&id);
-            Ok(())
-        } else {
-            Err(Error::NotExists(id, "learners"))
-        }
+        self.configuration.promote_learner(id)
     }
 
     /// Returns the minimal committed index for the cluster.
@@ -249,7 +334,7 @@ impl ProgressSet {
                 (accepted, total)
             },
         );
-        let quorum = self.voter_ids().len() / 2 + 1;
+        let quorum = self.configuration.voters().len() / 2 + 1;
         let rejected = total - accepted;
 
         info!(
@@ -271,7 +356,7 @@ impl ProgressSet {
     ///
     /// This should only be called by the leader.
     pub fn quorum_recently_active(&mut self, perspective_of: u64) -> bool {
-        let learners = self.learner_ids().clone();
+        let learners = self.configuration.learners().clone();
         let active = self
             .progress
             .iter_mut()
@@ -287,7 +372,12 @@ impl ProgressSet {
                 progress.recent_active = false;
                 active
             });
-        active >= (self.voter_ids().len() / 2 + 1)
+        active >= (self.configuration.voters().len() / 2 + 1)
+    }
+
+    /// Enter a joint into the given configuration.
+    pub fn begin_joint(&mut self, config: Configuration) -> Result<(), Error> {
+        unimplemented!();
     }
 }
 
